5bb6b25c949fc717886fa9a9e095e06dd7783405
muzilos <smm321@gmail.com>
2019-12-13 07:03:39 -0500

    implement db

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/Muzilos/golang-blockchain/blockchain"
 )
 
@@ -14,13 +11,13 @@ func main() {
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
 
-	for _, block := range chain.Blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
+	// for _, block := range chain.Blocks {
+	// 	fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+	// 	fmt.Printf("Data in Block: %s\n", block.Data)
+	// 	fmt.Printf("Hash: %x\n", block.Hash)
 
-		pow := blockchain.NewProof(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	// 	pow := blockchain.NewProof(block)
+	// 	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+	// 	fmt.Println()
+	// }
 }
